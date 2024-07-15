df78525baf037a603fac0ebb4e3e5899b8ea7472
Alexandre Direito <xandedsj@gmail.com>
2024-07-15 17:11:33 -0300

    Entity done!

---
+++ src/main/java/br/com/alexandredireito/desafio_todolist/Entity/Todo.java
@@ -0,0 +1,55 @@
+package br.com.alexandredireito.desafio_todolist.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean complete;
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
